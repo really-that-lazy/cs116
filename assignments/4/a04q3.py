469e03453e05ccbc16e1f13da0ccad4b3856afc3
bruce <johnnybgoode@rogers.com>
2017-10-10 12:47:38 -0400

    ...

---
+++ assignments/4/a04q3.py
@@ -0,0 +1,45 @@
+
+## Question 1
+
+def create_odds(target):
+    pass
+
+def build_special_list(n):
+    pass
+
+
+def divisibles(n):
+    pass
+
+
+def update_list(nlst, val, newval):
+    pass
+
+def mult_list (m1, m2):
+    pass
+
+## Question 2
+
+## contants for testing
+tweet1 = "#1:-@DanClark:-The party was amazing" 
+tweet19 = "#19:-@NatalyS:-Avoid 401 Toronto area at this time" 
+tweet50 = "#50:-@CBCNews:-How Canadian captain gave her team a speech" 
+tweet14 = "#14:-@DanClark:-The food was good"
+tweet15 = "#15:-@DaveLin:-Lucky you DanClark"
+tweets =   [tweet1, tweet19, tweet50, tweet14, tweet15]
+
+def search_tweets(tweets, tweeter):
+    pass
+
+
+## Question 3
+
+## A keycode is a list of length 2, [d, n] where
+##   d is an int from 0 - 9 representing a digit on a phone keypad and
+##   n is an int[>0], representing the number of times the key has
+##        been pressed. The value of n will be less than or equal to
+##        the number of symbols associated with d on a phone.
+
+def compose_msg(keypresses):
+    pass
+    
